9265a4ddc484fe5a3e9b44ea7097f2c31fa3fb4d
Danny0885 <danny0885@users.noreply.github.com>
2026-05-30 10:05:15 +0300

    Conditionally render VS divider only on desktop via JS

--- js/comparisons.js
+++ js/comparisons.js
@@ -118,7 +118,7 @@
       + '</div>'
       + '<div class="vs-grid">'
       + cardHTML(a, 1)
-      + '<div class="vs-divider"><span>VS</span></div>'
+      + (window.innerWidth > 768 ? '<div class="vs-divider"><span>VS</span></div>' : '')
       + cardHTML(b, 2)
       + '</div>'
       + '</div>';
